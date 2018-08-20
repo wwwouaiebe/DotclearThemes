23b76a745d761bd693e4bff11056f09054c2987b
wwwouaiebe <github.christian@guyette.be>
2018-08-20 11:27:19 +0200

    grunt
    Compressed css
    Temporary removed css vars
    Using grunt to generate dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,75 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
-			files: ['Gruntfile.js', 'src/**/*.js', 'tests/*.js'],
+			files: ['Gruntfile.js'],
+		},
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/common/sharedpictures/',
+						src: ['**'],
+						dest: 'dist/anthisnes/sharedpictures/'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/sharedpictures/',
+						src: ['**'],
+						dest: 'dist/ouaie/sharedpictures/'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/sharedscripts/',
+						src: ['**'],
+						dest: 'dist/anthisnes/sharedscripts/'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/sharedscripts/',
+						src: ['**'],
+						dest: 'dist/ouaie/sharedscripts/'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/tpl/',
+						src: ['**'],
+						dest: 'dist/anthisnes/tpl/'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/tpl/',
+						src: ['**'],
+						dest: 'dist/ouaie/tpl/'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/',
+						src: ['*'],
+						dest: 'dist/anthisnes/',
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'src/common/',
+						src: ['*'],
+						dest: 'dist/ouaie/',
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'src/anthisnes/',
+						src: ['**'],
+						dest: 'dist/anthisnes/'
+					},
+					{
+						expand: true,
+						cwd: 'src/ouaie/',
+						src: ['**'],
+						dest: 'dist/ouaie/'
+					},
+				],
+			}
 		},
 		cssmin: {
 			options: {
@@ -13,15 +81,16 @@ module.exports = function(grunt) {
 				files: {
 					'dist/ouaie/sharedstyles/presentationScheme.css': ['src/common/sharedstyles/reset.css', 'src/common/sharedstyles/presentationScheme.css', 'src/common/sharedstyles/menuScheme.css'],
 					'dist/ouaie/sharedstyles/standardColorScheme.css': ['src/common/sharedstyles/standardColorScheme.css'],
-					'dist/ouaie/sharedstyles/alternateColorScheme.css': ['src/common/sharedstyles/reset.css', 'src/common/sharedstyles/presentationScheme.css', 'src/common/sharedstyles/menuScheme.css'],
+					'dist/ouaie/sharedstyles/alternateColorScheme.css': ['src/common/sharedstyles/alternateColorScheme.css'],
 					'dist/anthisnes/sharedstyles/presentationScheme.css': ['src/common/sharedstyles/reset.css', 'src/common/sharedstyles/presentationScheme.css'],
 					'dist/anthisnes/sharedstyles/standardColorScheme.css': ['src/common/sharedstyles/standardColorScheme.css'],
-					'dist/anthisnes/sharedstyles/alternateColorScheme.css': ['src/common/sharedstyles/reset.css', 'src/common/sharedstyles/presentationScheme.css', 'src/common/sharedstyles/menuScheme.css'],
+					'dist/anthisnes/sharedstyles/alternateColorScheme.css': ['src/common/sharedstyles/alternateColorScheme.css']
 				}
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');	
-	grunt.registerTask('default', ['jshint', 'cssmin']);
+	grunt.registerTask('default', ['jshint', 'copy', 'cssmin']);
 };
